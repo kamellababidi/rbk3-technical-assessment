b9cf128fd6f75ee7bfc700caf62f265e55581b8b
kamellababidi <kamelallababidi@gmail.com>
2017-09-27 09:35:36 +0300

    made schema for user and job

--- back-end/models/Job.js
+++ back-end/models/Job.js
@@ -1,3 +1,14 @@
 // TODO: Create and export a mongoose model called `Job` that follows the description in the README
-
+var mongoose=require('mongoose');
+// create new instance of schema
+var Schema=mongoose.Schema;
+//make acctual schema which is jobs
+var jobschema = new Schema({
+	user_name: String ,
+	job : String
+});
+//make mode for this schema 
+var jobs=mongoose.model('jobs',jobschema);
+//export this schema to be shown to all files 
+module.exports=jobs;
 
--- back-end/models/User.js
+++ back-end/models/User.js
@@ -3,5 +3,18 @@ var exampleUser = {
   name: 'Taka',
   email: 'taka@taka.com'
 };
+//require  mongoose module 
+var mongoose=require('mongoose');
+// create new instance of schema
+var Schema=mongoose.Schema;
+//make acctual schema which is users
+var usersschema = new Schema({
+	name: String ,
+	email : String
+});
+//make mode for this schema 
+var users=mongoose.model('users',usersschema);
+//export this schema to be shown to all files 
+module.exports=users;
 
 
