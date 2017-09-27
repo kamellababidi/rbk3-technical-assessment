57285491d6fad7273194f7bd0dc5ed8ea67b85d3
kamellababidi <kamelallababidi@gmail.com>
2017-09-27 14:42:29 +0300

    finish with post /api/users

--- rest-api/index.js
+++ rest-api/index.js
@@ -7,11 +7,17 @@ app.use(bodyParser.json());
 
 // YOUR CODE BELOW
 app.get('/api/users',function(req,res){
+	res.status(200)
 	res.send(JSON.stringify(Users.getAll()));
 })
-// app.post('/api/users',function(){
-	
-// })
+app.post('/api/users',function(req,res){
+	 var name1=req.body.name;
+	 var email1=req.body.email;
+	 var x={name:name1,email:email1}
+	 Users.addOne({name:name1,email:email1})
+	 res.status(201)
+	 res.send(JSON.stringify(x))
+})
 
 // Do not touch this invocation of the `listen` method
 app.listen('8888', function () {
