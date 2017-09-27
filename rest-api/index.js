181990f78abd034544cbe41a5a361977ff84d779
kamellababidi <kamelallababidi@gmail.com>
2017-09-27 14:58:49 +0300

    finish with get to specific id

--- rest-api/index.js
+++ rest-api/index.js
@@ -18,7 +18,14 @@ app.post('/api/users',function(req,res){
 	 res.status(201)
 	 res.send(JSON.stringify(x))
 })
-
+app.get('/api/users/:tagId',function(req,res){
+	//req.params.tagId
+	//res.status(200)
+	var p=parseInt(req.params.tagId);  
+	var x=Users.getOne(p);
+	res.status(200)
+	res.send(JSON.stringify(x));
+})
 // Do not touch this invocation of the `listen` method
 app.listen('8888', function () {
   console.log('listening on 8888');
